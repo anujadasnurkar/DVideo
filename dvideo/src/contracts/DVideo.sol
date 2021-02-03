aaabd80a219160f27736c154ecdeef70c0f07745
anujadasnurkar <59170426+anujadasnurkar@users.noreply.github.com>
2021-02-03 12:28:17 +0530

    Add files via upload

---
+++ dvideo/src/contracts/DVideo.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.16;
+
+contract DVideo {
+  uint public videoCount = 0;
+  string public name = "DVideo";
+  mapping(uint => Video) public videos;
+
+  struct Video {
+    uint id;
+    string hash;
+    string title;
+    address author;
+  }
+
+  event VideoUploaded(
+    uint id,
+    string hash,
+    string title,
+    address author
+  );
+
+  function uploadVideo(string memory _videoHash, string memory _title) public {
+    require(bytes(_videoHash).length>0);
+    require(bytes(_title).length>0);
+    require(msg.sender!=address(0));
+    
+    videoCount++;
+    videos[videoCount] = Video(videoCount, _videoHash, _title, msg.sender);
+
+    emit VideoUploaded(videoCount, _videoHash, _title, msg.sender);
+
+  }
+}
